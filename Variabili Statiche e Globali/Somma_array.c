d6a9d8bb6a029736a76ca5af92cd12bcd1e484d3
Ali Haider Maqsood <alihaider.maqsood@gmail.com>
2019-05-20 20:09:25 +0200

    Esercizio impossibile

---
+++ Variabili Statiche e Globali/Somma_array.c
@@ -0,0 +1,134 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int valore_minimo(int a[], int b[], int indiciA[], int indiciB[], int DIM);
+
+int main(void){
+  srand(time(NULL));
+  int DIM;
+  printf("Inserire la grandezza dei 2 array: ");
+  scanf("%i", &DIM);
+  int A[DIM], B[DIM], C[DIM*2];
+  int indexA[DIM], indexB[DIM];
+
+  for(int i = 0 ; i < DIM ; i++){
+
+    A[i] = (rand() % DIM) + 1;
+    B[i] = (rand() % DIM) + 1;
+
+  } //Riempimento
+
+  for( int i = 0 ; i < DIM*2 ; i++ ){
+
+    C[i] = valore_minimo(A, B, indexA, indexB, DIM);
+
+  }
+
+  printf("\nContenuto dell'array A");
+  for( int i = 0 ; i < DIM ; i++ ){
+
+    if( i % 10 == 0 ){
+      printf("\n");
+    }
+
+    printf("[%i]:%i ", i, A[i]);
+  } //Stampa A
+
+  printf("\n\nContenuto dell'array B");
+  for( int i = 0 ; i < DIM ; i++ ){
+
+    if( i % 10 == 0 ){
+      printf("\n");
+    }
+
+    printf("[%i]:%i ", i, B[i]);
+  } //Stampa B
+
+  printf("\n\nContenuto dell'array C");
+  for( int i = 0 ; i < DIM*2 ; i++ ){
+
+    if( i % 10 == 0 ){
+      printf("\n");
+    }
+
+    printf("[%i]:%i ", i, C[i]);
+  } //Stampa C
+
+  printf("\n");
+
+  return 0;
+}
+
+int valore_minimo(int a[], int b[], int indiciA[], int indiciB[], int DIM){
+  int minimoA, indiceA, minimoB, indiceB;
+  int static kA = 0, kB = 0;
+  int bool=1;
+
+  for( int i = 0 ; i < DIM-1 ; i++ ){
+
+    for( int zA = 0 ; zA < kA ; zA++ ){
+      if( i != indiciA[zA] ){
+        bool = 1;
+      } else {
+        bool = 0;
+        break;
+      }
+    }
+    for( int zB = 0 ; zB < kB ; zB++ ){
+      if( i != indiciB[zB] && bool == 1){
+        bool = 1;
+      } else {
+        bool = 0;
+        break;
+      }
+    }
+    for( int zA = 0 ; zA < kA ; zA++ ){
+      if( i+1 != indiciA[zA] ){
+        bool = 1;
+      } else {
+        bool = 0;
+        break;
+      }
+    }
+    for( int zB = 0 ; zB < kB ; zB++ ){
+      if( i+1 != indiciB[zB] && bool == 1){
+        bool = 1;
+      } else {
+        bool = 0;
+        break;
+      }
+    }
+
+
+    if( bool == 1 ){
+
+      minimoA = a[i];
+      minimoB = b[i];
+
+      if( minimoA > a[i] ){
+        minimoA = a[i];
+        indiceA = i;
+      }
+      if( minimoB > b[i] ){
+        minimoB = b[i];
+        indiceB = i;
+      }
+
+    }
+
+  }
+
+  if( minimoA < minimoB ){
+    indiciA[kA] = indiceA;
+    kA++;
+    //printf("\n\nMINIMO A: %i, %i, %i\n\n", minimoA, i, j);
+    return minimoA;
+  } else {
+    indiciB[kB] = indiceB;
+    kB++;
+    //printf("\n\nMINIMO B: %i, %i , %i\n\n", minimoB, i, j);
+    return minimoB;
+  }
+
+}
