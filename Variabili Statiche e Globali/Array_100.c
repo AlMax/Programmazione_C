b4d72222a56c1600b18a0fa800a60d0d06c21426
Ali Haider Maqsood <alihaider.maqsood@gmail.com>
2019-05-20 10:46:21 +0200

    Variabili statiche e globali: Esercizio 1

---
+++ Variabili Statiche e Globali/Array_100.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+void verifica(int array[], int length); //Stampa i numeri che sono maggiori o uguali alla somma dei numeri successivi nell'array
+
+int DIM=100;
+
+int main(void){
+  srand(time(NULL));
+  int array[DIM];
+
+  for(int i = 0 ; i < DIM ; i++){
+
+    array[i] = rand() % 101;
+
+  } //Riempimento
+
+  printf("Contenuto dell'array");
+  for( int i = 0 ; i < DIM ; i++ ){
+
+    if( i % 10 == 0 ){
+      printf("\n");
+    }
+
+    printf("[%i]:%i ", i, array[i]);
+  } //Stampa
+
+  printf("\nNumeri Verificati\n");
+  verifica(array,DIM);
+
+  return 0;
+}
+
+void verifica(int array[], int length){
+  int somma;
+
+  for( int i = 0 ; i < DIM-1 ; i++ ){
+
+    somma=0;
+    for( int j = i + 1 ; j < DIM ; j++ ){
+      somma += array[j];
+    }
+
+    if(array[i] > somma){
+      printf("[%i]:%i ", i, array[i]);
+    }
+
+  }
+}
